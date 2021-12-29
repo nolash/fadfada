1ef63631702ea886b59c050d7bbb674ced77d502
nolash <dev@holbrook.no>
2021-12-29 10:40:51 +0000

    WIP implement sha256 validator for content

--- src/validator.rs
+++ src/validator.rs
@@ -1,3 +1,5 @@
+use log::debug;
+
 use super::resolver::{Resolver, Digest, Signature};
 
 /// The [Validator] is optionally used with a result from a [Source](crate:.source::Source) to verify the origin of
@@ -5,7 +7,7 @@ use super::resolver::{Resolver, Digest, Signature};
 pub trait Validator {
 
     /// Returns `true` if [Signature] can be verified against the [Digest] of content.
-    fn verify(&self, digest: &Digest, signature: &Signature) -> bool;
+    fn verify(&self, digest: &Digest, content: Option<&Vec<u8>>, signature: Option<&Signature>) -> bool;
 }
 
 pub struct Sha256ImmutableValidator<'r> {
@@ -13,7 +15,8 @@ pub struct Sha256ImmutableValidator<'r> {
 }
 
 impl<'a> Validator for Sha256ImmutableValidator<'a> {
-    fn verify(&self, _digest: &Digest, _signature: &Signature) -> bool {
+    fn verify(&self, digest: &Digest, _content: Option<&Vec<u8>>, _signature: Option<&Signature>) -> bool {
+        debug!("verifying digest {:?}", digest);
         true
     }
 }
@@ -23,7 +26,7 @@ pub struct NoopValidator {
 }
 
 impl Validator for NoopValidator {
-    fn verify(&self, _digest: &Digest, _signature: &Signature) -> bool {
+    fn verify(&self, _digest: &Digest, _content: Option<&Vec<u8>>, _signature: Option<&Signature>) -> bool {
         true
     }
 }
